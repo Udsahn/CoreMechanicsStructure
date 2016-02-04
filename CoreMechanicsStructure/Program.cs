9a26657233bd0e9b75dcfbc66d09ca531bbb5fbe
Udsahn <udsahn@gmail.com>
2016-02-04 18:10:25 -0500

    Initial Commit.

---
+++ CoreMechanicsStructure/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreMechanicsStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Don't simply close the window upon opening.
+            Console.Write("Press any key to close the application...");
+            Console.ReadKey();
+        }
+    }
+}
